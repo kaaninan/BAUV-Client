cac8c4277882a149a169aa3c02e0bbddaeedefe6
Kagan Inan <contact@kaaninan.net>
2022-09-08 02:31:08 +0300

    map alpha

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import store from './store/store.js'
 import router from './router/index.js'
 import VueGoogleMaps from '@fawmi/vue-google-maps'
 
+import 'maplibre-gl/dist/maplibre-gl.css';
+
 const app = createApp(App);
 app.use(store)
 app.use(router)
