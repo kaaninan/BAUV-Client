22f0752764b6f4248ce3965fdb22dcde617041c5
Kagan Inan <contact@kaaninan.net>
2022-10-04 17:43:05 +0300

    attitude wip #17

--- src/components/navigation/Attitude.vue
+++ src/components/navigation/Attitude.vue
@@ -7,29 +7,59 @@
 		<div class="_content">
 			<div class="outline">
 				
-				<div class="bankScaleLong">
-					<div v-for="(n, i) in bankScalesLong" :key="i">
-						<div class="line" :style="{transform: 'rotate(' + n + 'deg)', top: calcTop(n, 15), left: calcLeft(n, 4)}"></div>
+				<!-- Bank Scales -->
+				<div style="position: relative; width: 100%; height: 100%; transform: rotate(0deg);">
+					<div class="bankScaleLong">
+						<div v-for="(n, i) in bankScalesLong" :key="i">
+							<div class="line" :style="{transform: 'rotate(' + n + 'deg)', top: calcTop(n, 15), left: calcLeft(n, 4)}"></div>
+						</div>
 					</div>
-				</div>
-				
-				<div class="bankScaleShort">
-					<div v-for="(n, i) in bankScalesShort" :key="i">
-						<div class="line" :style="{transform: 'rotate(' + n + 'deg)', top: calcTop(n, 7.5), left: calcLeft(n, 2)}"></div>
+					
+					<div class="bankScaleShort">
+						<div v-for="(n, i) in bankScalesShort" :key="i">
+							<div class="line" :style="{transform: 'rotate(' + n + 'deg)', top: calcTop(n, 7.5), left: calcLeft(n, 2)}"></div>
+						</div>
+					</div>
+
+					<div class="bankScaleTriangle">
+						<div v-for="(n, i) in bankScalesTriangle" :key="i">
+							<div class="line" :style="{transform: 'rotate(' + n + 'deg)', top: calcTop(n, 8), left: calcLeft(n, 8)}"></div>
+						</div>
+					</div>
+					
+					<div class="bankScaleBigTriangle">
+						<div v-for="(n, i) in bankScalesBigTriangle" :key="i">
+							<div class="line" :style="{transform: 'rotate(' + n + 'deg)', top: calcTop(n, 15), left: calcLeft(n, 15)}"></div>
+						</div>
 					</div>
 				</div>
 
-				<div class="bankScaleTriangle">
-					<div v-for="(n, i) in bankScalesTriangle" :key="i">
-						<div class="line" :style="{transform: 'rotate(' + n + 'deg)', top: calcTop(n, 7.5), left: calcLeft(n, 6)}"></div>
+				<!-- Bank Scale Pointer -->
+				<div class="bankScalePointer">
+					<img class="pointer" src="@/assets/images/pointer.png" :style="{top: calcTop(0, 15), left: calcLeft(0, 13)}">
+				</div>
+
+				<!-- Pitch Degree Markings -->
+				<div class="pitchScale">
+					<div class="pitchScaleLong">
+						<div v-for="(n, i) in pitchScalesLong" :key="i">
+							<div class="leftText" :style="{top: 'calc(' + calcPitchLine(n * -1) + ' - 12.5px)'}">{{Math.abs(n)}}</div>
+							<div class="line" :style="{top: calcPitchLine(n)}"></div>
+							<div class="rightText" :style="{top: 'calc(' + calcPitchLine(n * -1) + ' - 12.5px)'}">{{Math.abs(n)}}</div>
+						</div>
+					</div>
+					<div class="pitchScaleShort">
+						<div v-for="(n, i) in pitchScalesShort" :key="i">
+							<div class="line" :style="{top: calcPitchLine(n)}"></div>
+						</div>
+					</div>
+					<div class="pitchScaleSoLong">
+						<div class="line" :style="{top: calcPitchLine(0)}"></div>
 					</div>
 				</div>
-				
-				<div class="bankScaleBigTriangle">
-					<div v-for="(n, i) in bankScalesBigTriangle" :key="i">
-						<div class="line" :style="{transform: 'rotate(' + n + 'deg)', top: calcTop(n, 15), left: calcLeft(n, 10)}"></div>
-					</div>
-				</div>
+
+
+
 			</div>
 		</div>
 	</div>
@@ -44,6 +74,9 @@ export default {
 			bankScalesShort: [340, 350, 10, 20],
 			bankScalesTriangle: [315, 45],
 			bankScalesBigTriangle: [0],
+
+			pitchScalesLong: [10, 20, -10, -20],
+			pitchScalesShort: [5, 15, -5, -15],
 		}
 	},
 	methods: {
@@ -52,6 +85,9 @@ export default {
 		},
 		calcLeft(i, diff) {
 			return 'calc(' + this.map(Math.cos(this.toRadians(this.map(i, 0, 90, 90, 0))), 0, 1, 50, 100) + '% - ' + diff + 'px)';
+		},
+		calcPitchLine(i){
+			return this.map(i, -20, 20, 25, 75) + '%';
 		},
 		map(x, in_min, in_max, out_min, out_max) {
 			return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
@@ -154,9 +190,9 @@ export default {
 
 ._containerAttitude .bankScaleTriangle .line{
 	position: absolute;
-	border-left: 6px solid transparent;
-	border-right: 6px solid transparent;
-	border-top: 15px solid white;
+	border-left: 8px solid transparent;
+	border-right: 8px solid transparent;
+	border-top: 16px solid white;
 }
 
 ._containerAttitude .bankScaleBigTriangle{
@@ -171,80 +207,111 @@ export default {
 
 ._containerAttitude .bankScaleBigTriangle .line{
 	position: absolute;
-	border-left: 10px solid transparent;
-	border-right: 10px solid transparent;
+	border-left: 15px solid transparent;
+	border-right: 15px solid transparent;
 	border-top: 30px solid white;
 }
 
-
-
-
-
-
-
-
-
-._containerAttitude .directions{
+._containerAttitude .bankScalePointer{
 	position: absolute;
-	top: 75px;
-	left: 75px;
-	right: 75px;
-	bottom: 75px;
+	top: 50px;
+	left: 50px;
+	right: 50px;
+	bottom: 50px;
 	border-radius: 1000px;
-	background-color: transparent;
-	/* border: 1px solid blue; */
+	/* background-color: rosybrown; */
 }
 
-._containerAttitude .directionItem{
+._containerAttitude .bankScalePointer .pointer{
+	position: absolute;
+	width: 26px;
+	height: 30px;
+	/* border: 1px solid red; */
+}
+
+
+
+
+._containerAttitude .pitchScale{
+	
+}
+._containerAttitude .pitchScale .pitchScaleLong{
+	position: absolute;
+	top: 15px;
+	left: 15px;
+	right: 15px;
+	bottom: 15px;
+	display: flex;
+	justify-content: center;
+	/* align-items: center; */
+}
+
+._containerAttitude .pitchScale .pitchScaleLong .line{
+	position: absolute;
+	width: 70px;
+	left: calc(50% - 35px);
+	height: 3px;
+	background-color: white;
+}
+._containerAttitude .pitchScale .pitchScaleLong .rightText{
+	position: absolute;
+	width: 25px;
+	height: 25px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	left: calc(50% + (35px + 5px));
+	font-size: 16px;
+}
+._containerAttitude .pitchScale .pitchScaleLong .leftText{
+	position: absolute;
+	width: 25px;
+	height: 25px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	left: calc(50% - (35px + 30px));
+	font-size: 16px;
+}
+
+._containerAttitude .pitchScale .pitchScaleShort{
+	position: absolute;
+	top: 15px;
+	left: 15px;
+	right: 15px;
+	bottom: 15px;
+	display: flex;
+	justify-content: center;
+	/* align-items: center; */
+}
+
+._containerAttitude .pitchScale .pitchScaleShort .line{
 	position: absolute;
 	width: 40px;
-	height: 20px;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	font-size: 1.5rem;
-	border-radius: 5px;
-	/* border: 1px solid green; */
+	left: calc(50% - 20px);
+	height: 3px;
+	background-color: white;
 }
-._containerAttitude .directionItemSmall{
+._containerAttitude .pitchScale .pitchScaleSoLong{
 	position: absolute;
-	width: 40px;
-	height: 20px;
+	top: 15px;
+	left: 15px;
+	right: 15px;
+	bottom: 15px;
 	display: flex;
 	justify-content: center;
-	align-items: center;
-	font-size: 1rem;
-	border-radius: 5px;
-	/* border: 1px solid green; */
+	/* align-items: center; */
 }
 
-._containerAttitude .borderBack{
+._containerAttitude .pitchScale .pitchScaleSoLong .line{
 	position: absolute;
-	top: 0px;
-	left: 0px;
-	right: 0px;
-	bottom: 0px;
-	border-radius: 1000px;
-	background-color: transparent;
-	border: 3vw solid navy;
-	z-index: 1;
+	width: 100%;
+	/* left: calc(50% - 100px); */
+	height: 2px;
+	background-color: white;
 }
 
-._containerAttitude .headingContainer{
-	/* background-color: red; */
-	position: absolute;
-	top: 50%;
-	left: 50%;
-	transform: translate(-50%, -50%) rotate(-45deg);
-	width: 100px;
-	height: 100px;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	text-align: center;
-	font-weight: 600;
-	color: greenyellow;
-	font-size: 1.5rem;
-}
+
+
 
 </style>
